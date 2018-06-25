b8e8a025840fc251d625cb70d0ace7e1b6de897d
Jeff Bryner <jeff@jeffbryner.com>
2018-06-25 16:34:57 -0700

    test removing the client side update

--- methods/update-quill-client.js
+++ methods/update-quill-client.js
@@ -1,9 +1,13 @@
-Meteor.methods({
-  'updateQuill': function(collectionName, docId, field, delta, newContents) {
-    this.unblock();
-    var collection = Mongo.Collection.get(collectionName);
-    var updateObj = {$set: {}};
-    updateObj["$set"][field + "Delta"] = newContents;
-    collection.update({_id: docId}, updateObj)
-  }
-})
+if (Meteor.isClient) {
+    // Meteor.methods({
+    // 'updateQuill': function(collectionName, docId, field, delta, newContents) {
+    //     console.log('updateQuill client called',newContents)
+    //     this.unblock();
+    //     var collection = Mongo.Collection.get(collectionName);
+    //     var updateObj = {$set: {}};
+    //     updateObj["$set"][field + "Delta"] = newContents;
+    //     console.log(updateObj);
+    //     collection.update({_id: docId}, updateObj)
+    // }
+    // })
+};
